751cae7799a36276c2cb9c038a04cb22f9241bbc
Catherine Liang <46097972+reliang@users.noreply.github.com>
2019-02-09 16:17:24 -0500

    Update Channel.java

--- Channel.java
+++ Channel.java
@@ -1,27 +1,47 @@
-package femmehack;
 import java.util.*;
 
 public class Channel {
-    static int userID = 1;
-    static int taskID = 1;
-    static HashMap<Integer,User> userList=new HashMap<Integer,User>();
-    static HashMap<Integer,Task> taskList=new HashMap<Integer,Task>();
+    static int userID = 0;
+    static int taskID = 0;
+    static HashMap<Integer,User> userList = new HashMap<Integer, User>(); //ID to user
+    static HashMap<String, User> signIn = new HashMap<String, User>(); //email to user
+    static HashMap<Integer,Task> taskList = new HashMap<Integer,Task>(); //ID to task
     
-    static void makeUser(String name, String email) {
-        User user = new User(name, userID++, email);
-        userList.add(user.getID(), user):
+    // matches email, if email exists, tries to sign in by matching username
+    // if not, makes new user with email 
+    static User signIn(String name, String email) {
+    	if(signIn.containsKey(email)) {
+    		User curr = signIn.get(email);
+    		if (curr.name.equals(name)) {
+    			return curr;
+    		} else {
+    			return null;
+    		}
+    	} else {
+    		User curr = makeUser(name, email);
+    		return curr;
+    	}
     }
     
-    static void makeTask(User owner,String request, int time) {
+    static User makeUser(String name, String email) {
+        User user = new User(name, userID++, email);
+        userList.put(user.id, user);
+        signIn.put(email, user);
+        return user;
+    }
+    
+    static int makeTask(User owner, String request, int time) {
         Task task = new Task(owner, request, time, taskID++);
-        userList.add(user.getID(), user):
+        taskList.put(task.id, task);
+        return task.id;
     }
     
     static void removeTask(int id) {
         taskList.remove(id);
     }
     
-    
-    
+    static User getUser(int id) {
+    	return (User) userList.get(id);
+    }
     
 }
