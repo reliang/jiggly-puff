79bbcb7e9c5ac59d02e23ebff306515dafe53dd9
hotcake101 <44275713+hotcake101@users.noreply.github.com>
2019-02-09 14:56:41 -0500

    Update Task.java

--- Task.java
+++ Task.java
@@ -1,14 +1,30 @@
 package femmehack;
 
 public class Task {
+    String name;
     int id;
     int time;
     User owner;
     User doer;
-    boolean owenerConfirm;
+    boolean taken;
+    boolean ownerConfirm;
     boolean doerConfirm;
-    boolean isConfirmed() {
-        return owenerConfirm && doerConfirm;
+    
+    public Task(User owner,String request, int time, int id) {
+        this.id = id;
+        this.owner = owner;
+        this.time = time;
+        taken = false;
+        ownerConfirm = false;
+        doerConfirm = false;
+    }
+    
+    void isConfirmed(int id) {
+        if (ownerConfirm && doerConfirm) {
+            Channel.removeTask(id);
+            owner.deleteTask(id);
+            doer.deleteTask(id);
+        };
     }
 
 }
